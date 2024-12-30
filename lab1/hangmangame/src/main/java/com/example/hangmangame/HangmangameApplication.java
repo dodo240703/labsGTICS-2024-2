0d56e68240b95d72ac86d73fa4314236e4129485
dodo240703 <a20202396@pucp.edu.pe>
2024-12-29 21:20:43 -0500

    Creando proyecto

---
+++ lab1/hangmangame/src/main/java/com/example/hangmangame/HangmangameApplication.java
@@ -0,0 +1,13 @@
+package com.example.hangmangame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangmangameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangmangameApplication.class, args);
+	}
+
+}
---
+++ lab1/hangmangame/src/test/java/com/example/hangmangame/HangmangameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hangmangame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangmangameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
